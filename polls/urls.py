22847e66f3a38f1f760dfe30fdf425b689099cec
Jacob Wolf <jacob.h.wolf@gmail.com>
2021-01-28 16:34:56 +0800

    Remove past polls view

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('past/', views.PastView.as_view(), name='past'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:day_id>/vote/', views.vote, name='vote'),
 ]
