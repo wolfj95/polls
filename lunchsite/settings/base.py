5aa0648f1f75dff9760cb1e50ab1d02dc5be0c75
Jacob Wolf <jacob.h.wolf@gmail.com>
2021-02-03 13:50:56 +0800

    Change file structure
    Move all resources for polls app into polls/. Removed explicit setting of static directory

--- lunchsite/settings/base.py
+++ lunchsite/settings/base.py
@@ -123,9 +123,9 @@ USE_TZ = True
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+    # BASE_DIR / "static",
+# ]
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = BASE_DIR / 'staticfiles'  #. os.path.join(BASE_DIR, 'staticfiles')
